65bc27e739b2e9302e51fba296360983ce2b9a59
Samir P Nimgade <samirnimgade76@gmail.com>
2026-03-12 22:10:59 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,6 @@
+// Example: Add a "Highlight" border to the video player
+const player = document.querySelector('.html5-video-player');
+if (player) {
+    player.style.border = '5px solid #FF0000';
+    console.log("Feature Activated!");
+}
